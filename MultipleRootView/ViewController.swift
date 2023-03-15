400f48672afa4c3fd122936da4854b91ec570f8a
Bonoogi <bnoo1333@gmail.com>
2023-03-15 10:39:34 +0900

    Initial Commit

---
+++ MultipleRootView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultipleRootView
+//
+//  Created by 구본욱 on 2023/03/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
